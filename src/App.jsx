01b6d6e41fc9927080f27fb0abd5c53b2282db0d
yeasinshakil <shakilrock1998@gmail.com>
2023-09-07 12:12:24 +0600

    project setup and header navigation bar

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+
+
+import Home from './components/Home/Home';
+import Category from './components/Category/Category';
+import SingleProduct from './components/SingleProduct/SingleProduct';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import NewsLetter from './components/Footer/Newsletter/Newsletter';
+import AppContext from "./utils/context";
+
+function App() {
+
+
+  return (
+    <BrowserRouter>
+      <AppContext>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/category/:id" element={<Category />}></Route>
+          <Route path="/product/:id" element={<SingleProduct />}></Route>
+        </Routes>
+        <NewsLetter />
+        <Footer />
+      </AppContext>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,29 @@
+import "./Header.scss";
+import {TbSearch} from 'react-icons/tb'
+import {AiOutlineHeart} from 'react-icons/ai'
+import {CgShoppingCart} from 'react-icons/cg'
+
+const Header = () => {
+    return <div>
+        <header className="main-header">
+            <div className="header-content">
+                <ul className="left">
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Categories</li>
+                </ul>
+                <div className="center">EMMAJOHNSTORE</div>
+                <div className="right">
+                    <TbSearch />
+                    <AiOutlineHeart />
+                    <span className=" cart-icon">
+                        <CgShoppingCart />
+                        <span>5</span>
+                    </span>
+                </div>
+            </div>
+        </header>
+    </div>;
+};
+
+export default Header;
