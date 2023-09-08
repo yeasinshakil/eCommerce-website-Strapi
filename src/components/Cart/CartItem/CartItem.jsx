432c8b41de33323001fe43bc73b8fce7f79f382a
yeasinshakil <shakilrock1998@gmail.com>
2023-09-08 22:30:45 +0600

    cart section design

--- src/components/Cart/CartItem/CartItem.jsx
+++ src/components/Cart/CartItem/CartItem.jsx
@@ -1,6 +1,30 @@
-import "./CartItem.scss";
+import './CartItem.scss'
+import prod from '../../../assets/products/headphone-prod-2.webp'
+import { MdClose } from "react-icons/md";
 const CartItem = () => {
-    return <div>Cart item</div>;
+    return (
+        <div className="cart-products">
+            <div className="cart-product">
+                <div className="image-container">
+                    <img src={prod} alt="" />
+                </div>
+                <div className="prod-details">
+                    <span className="name">Product name</span>
+                    <MdClose />
+                    <div className="quantity-buttons">
+                        <span>-</span>
+                        <span>2</span>
+                        <span>+</span>
+                    </div>
+                    <div className="text">
+                        <span>3</span>
+                        <span>x</span>
+                        <span>&#2547; 545</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default CartItem;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,9 +3,10 @@ import { TbSearch } from 'react-icons/tb'
 import { AiOutlineHeart } from 'react-icons/ai'
 import { CgShoppingCart } from 'react-icons/cg'
 import { useEffect, useState } from "react";
-
+import Cart from '../Cart/Cart'
 const Header = () => {
-    const [scrolled, setScrolled] = useState(false)
+    const [showCart, setShowCart] = useState(false);
+    const [scrolled, setScrolled] = useState(false);
     const handleScroll = () => {
         const offset = window.scrollY;
         if (offset > 200) {
@@ -15,7 +16,7 @@ const Header = () => {
     useEffect(() => {
         window.addEventListener('scroll', handleScroll)
     }, [])
-    return <div>
+    return (<>
         <header className={`main-header ${scrolled ? 'sticky-header' : ''}`}>
             <div className="header-content">
                 <ul className="left">
@@ -25,16 +26,18 @@ const Header = () => {
                 </ul>
                 <div className="center">EMMAJOHNSTORE</div>
                 <div className="right">
-                    <TbSearch />
-                    <AiOutlineHeart />
-                    <span className=" cart-icon">
+                    <span><TbSearch /></span>
+                    <span><AiOutlineHeart /></span>
+                    <span className=" cart-icon" onClick={()=> setShowCart(true)}>
                         <CgShoppingCart />
                         <span>5</span>
                     </span>
                 </div>
             </div>
         </header>
-    </div>;
+       
+        {showCart && <Cart />}
+    </>);
 };
 
 export default Header;
