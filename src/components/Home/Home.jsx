e9727e1c13155cf48eea2ad7aa32c7935f7511a8
yeasinshakil <shakilrock1998@gmail.com>
2023-09-08 12:37:42 +0600

    category section

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,16 @@
 import Newsletter from "../Footer/Newsletter/Newsletter";
 import Banner from "./Banner/Banner";
+import Category from "./Category/Category";
 import "./Home.scss";
 const Home = () => {
     return (
         <div>
             <Banner />
+            <div className="main-content">
+                <div className="layout">
+                    <Category />
+                </div>
+            </div>
         </div>
     )
 };
