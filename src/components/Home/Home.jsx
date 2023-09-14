6ba34db8856dc513934d9e4ac0252e60bac56a76
yeasinshakil <shakilrock1998@gmail.com>
2023-09-14 09:11:49 +0600

    initialize strapi backend to the project

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,16 +1,25 @@
-import Newsletter from "../Footer/Newsletter/Newsletter";
 import Banner from "./Banner/Banner";
 import Category from "./Category/Category";
 import Products from '../Products/Products';
 import "./Home.scss";
+import { useEffect } from "react";
+import { fetchDataFromApi } from "../../utils/api";
+import axios from "axios";
 const Home = () => {
+    useEffect(() => {
+       getCategory()
+    }, [])
+
+    const getCategory = () => {
+        fetchDataFromApi("/api/categories?populate=*").then((res) => console.log(res))
+    }
     return (
         <div>
             <Banner />
             <div className="main-content">
                 <div className="layout">
                     <Category />
-                    <Products headingText='popular products'/>
+                    <Products headingText='popular products' />
                 </div>
             </div>
         </div>
