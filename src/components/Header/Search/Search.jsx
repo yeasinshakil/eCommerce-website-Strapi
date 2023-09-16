8a932d967d5cd117e75c3d6a793a92b4705ad39c
yeasinshakil <shakilrock1998@gmail.com>
2023-09-16 13:16:38 +0600

    Search Functionality implement

--- src/components/Header/Search/Search.jsx
+++ src/components/Header/Search/Search.jsx
@@ -1,7 +1,25 @@
+import React, { useState } from "react";
 import { MdClose } from "react-icons/md";
 import "./Search.scss";
-import prod from "../../../assets/products/headphone-prod-1.webp";
+import useFetch from "../../../hooks/useFetch";
+import { useNavigate } from "react-router-dom";
+
 const Search = ({ setSearchModal }) => {
+    const [query, setQuery] = useState("");
+    const navigate = useNavigate();
+
+    const onChange = (e) => {
+        setQuery(e.target.value);
+    };
+
+    let { data } = useFetch(
+        `/api/products?populate=*&filters[title][$contains]=${query}`
+    );
+
+    if (!query.length) {
+        data = null;
+    }
+
     return (
         <div className="search-modal">
             <div className="form-field">
@@ -9,37 +27,48 @@ const Search = ({ setSearchModal }) => {
                     autoFocus
                     type="text"
                     placeholder="Search for products"
+                    value={query}
+                    onChange={onChange}
                 />
                 <MdClose
                     className="close-btn"
                     onClick={() => setSearchModal(false)}
                 />
             </div>
-
             <div className="search-result-content">
-                
+                {!data?.data?.length && (
                     <div className="start-msg">
                         Start typing to see products you are looking for.
                     </div>
-                
+                )}
                 <div className="search-results">
-                    
+                    {data?.data?.map((item) => (
                         <div
-                            className="search-result-item">
+                            className="search-result-item"
+                            key={item.id}
+                            onClick={() => {
+                                navigate("/product/" + item.id);
+                                setSearchModal(false);
+                            }}
+                        >
                             <div className="image-container">
                                 <img
-                                    src={prod}
+                                    src={
+                                        import.meta.env.VITE_REACT_APP_STRIPE_APP_DEV_URL +
+                                        item.attributes.productImage.data[0].attributes.url
+                                    }
                                 />
                             </div>
                             <div className="prod-details">
                                 <span className="name">
-                                    Lorem ipsum dolor, sit amet consectetur adipisicing.
+                                    {item.attributes.title}
                                 </span>
                                 <span className="desc">
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus libero accusantium minus!
+                                    {item.attributes.description}
                                 </span>
                             </div>
                         </div>
+                    ))}
                 </div>
             </div>
         </div>
